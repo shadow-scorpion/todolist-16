4121446793bf600e6b5d8ee549dfcc6452814a5b
Alex <secret.teamplayer@gmail.com>
2025-12-13 21:05:24 +0200

    index re-export and improved handleServerNetworkError

--- src/common/utils/handleServerNetworkError.ts
+++ src/common/utils/handleServerNetworkError.ts
@@ -1,7 +1,9 @@
 import axios, { AxiosError } from "axios"
 import { z } from "zod"
+import { Dispatch } from "@reduxjs/toolkit"
+import { setAppErrorAC } from "@/app/app-slice.ts"
 
-export const handleServerNetworkError = (error: unknown): string => {
+export const handleServerNetworkError = (dispatch: Dispatch, error: unknown): string => {
   let errorMessage: string;
 
   switch (true) {
@@ -9,27 +11,28 @@ export const handleServerNetworkError = (error: unknown): string => {
     case axios.isAxiosError(error):
       const axiosError = error as AxiosError<any>;
       errorMessage = axiosError.response?.data?.message || axiosError.message;
+      dispatch(setAppErrorAC({ error: errorMessage }))
       break;
 
     // 2. Специфичная: Zod Error
     case error instanceof z.ZodError:
       const zodError = error as z.ZodError;
       errorMessage = `Zod error: look at console`;
+      dispatch(setAppErrorAC({ error: errorMessage }))
       console.log(zodError.issues)
       break;
 
     // 3. Общая: Native Error
     case error instanceof Error:
       errorMessage = `Error: ${(error as Error).message}`;
+      dispatch(setAppErrorAC({ error: errorMessage }))
       break;
 
     // 4. Fallback
     default:
-      try {
         errorMessage = JSON.stringify(error);
-      } catch (e) {
-        errorMessage = 'An unhandled, non-serializable error occurred';
-      }
+        dispatch(setAppErrorAC({ error: errorMessage }))
+
       break;
   }
 
