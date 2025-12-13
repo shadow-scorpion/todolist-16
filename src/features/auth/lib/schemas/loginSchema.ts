1ebe10782463407a60134cd32e963bcf2a10de70
Alex <secret.teamplayer@gmail.com>
2025-12-13 19:29:02 +0200

    loginSchema

--- src/features/auth/lib/schemas/loginSchema.ts
+++ src/features/auth/lib/schemas/loginSchema.ts
@@ -1,25 +1,9 @@
-import * as z from "zod";
-
-export type LoginInputs = z.infer<typeof loginSchema>
+import { z } from "zod"
 
 export const loginSchema = z.object({
   email: z.email(),
-  password: z.string()
-    .min(4, 'Too short password')
-    .max(10, 'Too long password'),
+  password: z.string().min(3),
   rememberMe: z.boolean()
 })
 
-
-
-// const User1 = z.object({
-//   email: z.email().max(20),
-//   password: z.string().min(3, 'Bro its too small').max(10, 'Made pass less'),
-//   rememberMe: z.boolean()
-// })
-//
-// type User = z.infer<typeof User1>
-//
-// const data: User = User1.parse({email: 'mailo-my@gmail.com', password: 1034, rememberMe: 'true'});
-//
-// console.log(data)
+export type LoginInputs = z.infer<typeof loginSchema>
